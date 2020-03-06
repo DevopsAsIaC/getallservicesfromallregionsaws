abeb8f26fddc0924b94625a85361897d1ba22dd0
Ramprasad GK <gkraam@gmail.com>
2020-03-06 16:12:58 +0530

    added support for rds

--- list_all_services_in_all_regions.py
+++ list_all_services_in_all_regions.py
@@ -1,25 +1,33 @@
-import boto3
+mport boto3
 import GenUtils
 
-regions= GenUtils.get_all_regions()
+#regions= GenUtils.get_all_regions()
 services=GenUtils.get_all_services()
-print (boto3.Session().get_credentials().access_key)
-print (boto3.Session().get_credentials().secret_key)
-for region in regions:
-	for service in services:
-		print(service,region)
-		bclient = boto3.client(service, region_name=region)
-		if (service == 'ec2'):
-			try:
-				response = bclient.describe_instances()
-			except:
-				print ("no"+ service +"in region " + region  )
-				continue
-			for reservation in response["Reservations"]:
-				for instance in reservation["Instances"]:
-                			if instance['State']['Name'] == 'running':
-                 	   			print(instance["InstanceId"])
-		elif (service == "s3"):
-			print("list s3 for s3" )	
-		else:
-			print("TODO ", service)
+
+for service in services:
+        available_regions = boto3.Session().get_available_regions(service)
+        for region in available_regions:
+                #print(service,region)
+                bclient = boto3.client(service, region_name=region)
+                if (service == 'ec2'):
+                        try:
+                                response = bclient.describe_instances()
+                        except:
+                                print ("no"+ service +"in region " + region  )
+                                continue
+                        for reservation in response["Reservations"]:
+                                for instance in reservation["Instances"]:
+                                        if instance['State']['Name'] == 'running':
+                                                print(region,service, instance["InstanceId"])
+                elif (service == "rds"):
+                        try:
+                                paginator = bclient.get_paginator('describe_db_instances').paginate()
+                                for page in paginator:
+                                        for dbinstance in page['DBInstances']:
+                                                print(region,service,"{DBInstanceClass}".format(**dbinstance))
+                        except:
+                                print ("no rds instances",region)
+
+
+                else:
+                        continue
